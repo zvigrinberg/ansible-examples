a490b82a675a1f8ede5d88b9a0f9f8e7ddb49cd6
Zvi Grinberg <zgrinber@redhat.com>
2023-03-05 15:24:04 +0200

    Add example of playbook that prints live lines of output of async invoked shell script
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

---
+++ async-task/script.sh
@@ -0,0 +1,10 @@
+echo "Start Of Script" 
+sleep 20
+echo "Middle of Script" 
+sleep 10
+echo "3 stage of  Script" 
+sleep 20
+echo "4 stage of  Script" 
+sleep 15
+echo "final stage of  Script" 
+sleep 3
